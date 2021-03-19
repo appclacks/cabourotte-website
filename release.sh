01d5c3d4c9df386816247e17a43276f0768cd183
mcorbin <corbin.math@gmail.com>
2021-03-19 20:19:51 +0100

    release v0.0.4

--- release.sh
+++ release.sh
@@ -1,6 +1,8 @@
 set -e
 tag=$1
 hugo
+git add .
+git commit -m "release ${tag}"
 git tag -a "${tag}" -m "release ${tag}"
 docker build -t mcorbin/cabourotte-website:${tag} .
 docker push mcorbin/cabourotte-website:${tag}
